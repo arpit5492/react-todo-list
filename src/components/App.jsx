3a4445a17eea82d50c6dfa91be02920fae280397
arpit5492 <arpitmallick99@gmail.com>
2024-01-31 03:29:51 -0800

    Added hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 function App() {
+
+  const [item, addItem] = useState({
+    itemName: ""
+  });  
+
+  function handleChange(event){
+    const {name, value} = event.target;
+
+    console.log(name);
+    console.log(value);
+
+    addItem((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      }
+    })
+  }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input type="text"
+           name="inputText"
+          //  value={item.itemName}
+           onChange={handleChange}
+        />
         <button>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          <li>{item.itemName}</li>
         </ul>
       </div>
     </div>
